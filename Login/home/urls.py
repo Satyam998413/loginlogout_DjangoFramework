9918c1623723ac045b46394c3ff8eb65944a4903
Satyam998413 <84256320+Satyam998413@users.noreply.github.com>
2022-03-05 17:14:42 +0530

    Day 7  I have created login and signup form

--- Login/home/urls.py
+++ Login/home/urls.py
@@ -1,11 +1,14 @@
-from django.urls import path,include
-from . import views
+from django.contrib import admin
+from django.urls import path, include
+from home import views
 
 urlpatterns = [
-    path("",views.index,name="home"),
+    path('', views.home, name="home"),
     path('contact', views.contact, name="contact"),
     path('about', views.about, name="about"),
     path('search', views.search, name="search"),
+    path('search', views.search, name="search"),
     path('signup', views.handleSignUp, name="handleSignUp"),
-    
+    path('login', views.handeLogin, name="handleLogin"),
+    path('logout', views.handelLogout, name="handleLogout"),
 ]
